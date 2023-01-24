76d5e7b0c6fe1fa803cb2ac6888cb81be3bfcb35
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-01-24 16:38:28 +0100

    Add pokemon list component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,25 @@
 <script>
+import PokemonList from './pokemon/PokemonList.vue';
 export default {
-name: 'AppMain'
+name: 'AppMain',
+components: {PokemonList}
 };
 
 </script>
 
 <template>
-  <main></main>
+  <main class="container">
+    <PokemonList></PokemonList>
+  </main>
 </template>
 
 <style lang="scss" scoped >
     @use '../assets/scss/style.scss' as *;
+
+    main {
+        border: 50px solid rgb(193, 0, 0);
+        border-radius: 20px;
+        min-height: 500px
+    }
+
 </style>
---
+++ src/components/pokemon/PokemonList.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+name: 'PokemonList'
+};
+
+</script>
+
+<template>
+  <section id="pokemon">lista card pokemon</section>
+</template>
+
+<style lang="scss" scoped >
+    @use '../../assets/scss/style.scss' as *;
+</style>
