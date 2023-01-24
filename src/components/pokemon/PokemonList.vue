679203b870f3b9aae622e677c8e9f3c4a68bfe5d
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-01-24 17:25:40 +0100

    Add pokemon API & hardcoded try

--- src/components/pokemon/PokemonList.vue
+++ src/components/pokemon/PokemonList.vue
@@ -1,12 +1,30 @@
 <script>
+import axios from 'axios'
 export default {
-name: 'PokemonList'
+name: 'PokemonList',
+data() {
+    return {
+        pokemons: []
+    }
+},
+created() {
+    axios.get('https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=10&page=1')
+    .then((res) => {
+        this.pokemons = res.data.docs;
+    })
+}
 };
 
 </script>
 
 <template>
-  <section id="pokemon">lista card pokemon</section>
+  <section id="pokemon">
+    <div class="row g-3 row-cols-3">
+        <div v-for="pokemon in pokemons" @key="pokemon.id">
+        {{ pokemon.name }}
+        </div>
+    </div>
+  </section>
 </template>
 
 <style lang="scss" scoped >
