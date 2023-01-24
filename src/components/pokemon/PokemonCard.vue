4d2583050c4b4d110ed07251b801a2296702bb7c
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-01-24 17:48:47 +0100

    Finish

---
+++ src/components/pokemon/PokemonCard.vue
@@ -0,0 +1,34 @@
+<script>
+// import axios from 'axios'
+export default {
+name: 'PokemonCard',
+props:{
+    image: String,
+    number: Number,
+    name: String,
+    type1: String,
+},
+data() {
+    
+},
+};
+
+</script>
+
+<template>
+ <div class="align-items-center p-4 card">
+                <img :src="image" alt="">
+                <div> #{{ number }} </div>
+                <h2> {{ name }} </h2>
+                <div class="fw-bold">  {{ type1 }} </div>
+            </div>
+</template>
+
+<style lang="scss" scoped >
+    @use '../../assets/scss/style.scss' as *;
+
+    img {
+        height: 100px;
+        width: 100px;
+    }
+</style>
--- src/components/pokemon/PokemonList.vue
+++ src/components/pokemon/PokemonList.vue
@@ -1,7 +1,9 @@
 <script>
 import axios from 'axios'
+import PokemonCard from './PokemonCard.vue'
 export default {
 name: 'PokemonList',
+components: {PokemonCard},
 data() {
     return {
         pokemons: []
@@ -20,9 +22,15 @@ created() {
 <template>
   <section id="pokemon">
     <div class="row g-3 row-cols-3">
-        <div v-for="pokemon in pokemons" @key="pokemon.id">
-        {{ pokemon.name }}
-        </div>
+        <PokemonCard 
+        v-for="pokemon in pokemons" 
+        :image="pokemon.imageUrl"
+        :number="pokemon.number"
+        :name="pokemon.name" 
+        :type1="pokemon.type1" 
+        @key="pokemon.id">
+        
+        </PokemonCard>
     </div>
   </section>
 </template>
