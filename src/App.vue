4436c970ebfce25b17cf909ccabcd5cc3cefac15
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-01-24 16:18:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppMain from './components/AppMain.vue'
+
+export default {
+    components: {AppMain}
+};
+
+</script>
+
+<template>
+  <header class="container d-flex justify-content-center">
+    <img id="logo-header" src="./assets/img/logo-pokedex.png" alt="">
+  </header>
+  <AppMain></AppMain>
+</template>
+
+<style lang="scss" >
+@use './assets/scss/style.scss' as *;
+
+#logo-header {
+  height: 180px;
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+name: 'AppMain'
+};
+
+</script>
+
+<template>
+  <main></main>
+</template>
+
+<style lang="scss" scoped >
+    @use './assets/scss/style.scss' as *;
+</style>
